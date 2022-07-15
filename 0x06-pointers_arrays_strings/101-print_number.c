e332838ad5c222c8de555ab49f5aae93740bfc65
anibrown <anibrownudo400@gmail.com>
2022-07-14 21:16:28 -0700

    101 again

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,46 +1,43 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_number -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
- * Return: Always 0.
- */
-void print_number(int n)
+* print_buffer - prints a buffer
+* @b: buffer.
+* @size: size of buffer.
+* Return: no return.
+*/
+
+void print_buffer(char *b, int size)
 {
-	unsigned int z;
-	int m, b;
-
-	b = 10;
-
-	if (n < 10 && n >= 0)
-	{
-		_putchar (n + '0');
-	}
-	else if (n > -10 && n < 0)
-	{
-		n = n - 2 * n;
-		_putchar('-');
-		_putchar (n + '0');
-	}
-
-	else
-	{
-		if (n < 0)
-		{
-			n = n * -1;
-			_putchar ('-');
-		}
-		z = n;
-	while (z / b > 9)
-	{
-		b = b * 10;
-	}
-	while (b > 0)
-	{
-		m = z / b;
-		z = z % b;
-		_putchar (m + '0');
-		b = b / 10;
-	}
-	}
+int j, k, l;
+if (size <= 0)
+printf("\n");
+else
+{
+for (j = 0; j < size; j += 10)
+{
+printf("%.8x:", j);
+for (k = j; k < j + 10; k++)
+{
+if (k % 2 == 0)
+printf(" ");
+if (k < size)
+printf("%.2x", *(b + k));
+else
+printf("  ");
+}
+printf(" ");
+for (l = j; l < j + 10; l++)
+{
+if (l >= size)
+break;
+if (*(b + l) < 32 || *(b + l) > 126)
+printf("%c", '.');
+else
+printf("%c", *(b + l));
+}
+printf("\n");
+}
+}
 }
