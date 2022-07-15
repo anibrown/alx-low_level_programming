4febcae677b08ae99411b67dab6dc48bf34280b7
anibrown <anibrownudo400@gmail.com>
2022-07-14 21:23:05 -0700

    103 fixed and tested

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,23 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(void)
+/**
+ * infinite_add - adds two numbers
+ * @n1: number to be added
+ * @n2: second number
+ * @r: result of the addition
+ * @size_r: size of r
+ *
+ * Return: 0;
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-  int n;
-  int a[5];
-  int *p;
-
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	*r = *n1 + *n2;
 }
